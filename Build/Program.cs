7fd641ddfe43619ed1dc545990e31c15d9592314
kmyuhkyuk <2451614940@qq.com>
2024-01-13 21:48:50 +0800

    Update
    Former-commit-id: a22bd9de1e18ead0759529d41b6d6447d0f59c89

--- Build/Program.cs
+++ Build/Program.cs
@@ -11,16 +11,17 @@ namespace Build
         private static void Main(string[] args)
         {
             var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var arg = args.ElementAtOrDefault(0);
 
             const string releasePath =
                 "R:\\Battlestate Games\\Client.0.13.5.3.26535\\BepInEx\\plugins\\kmyuhkyuk-GamePanelHUD";
 
-            Copy.CopyAssembly(args.ElementAtOrDefault(0), "Release", baseDirectory, Path.Combine(releasePath, "core"), new[]
+            Copy.CopyAssembly(arg, "Release", baseDirectory, Path.Combine(releasePath, "core"), new[]
             {
                 "GamePanelHUDCore"
             });
 
-            Copy.CopyAssembly(args.ElementAtOrDefault(0), "Release", baseDirectory, releasePath, new[]
+            Copy.CopyAssembly(arg, "Release", baseDirectory, releasePath, new[]
             {
                 "GamePanelHUDCompass",
                 "GamePanelHUDGrenade",
@@ -32,7 +33,7 @@ namespace Build
 
             const string unityEditorPath = "C:\\Users\\24516\\Documents\\GamePanelHUD\\Assets\\Managed";
 
-            Copy.CopyAssembly(args.ElementAtOrDefault(0), "UNITY_EDITOR", baseDirectory, unityEditorPath,
+            Copy.CopyAssembly(arg, "UNITY_EDITOR", baseDirectory, unityEditorPath,
                 new[]
                 {
                     "GamePanelHUDCore",
