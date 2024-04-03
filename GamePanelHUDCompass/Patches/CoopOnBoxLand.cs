406229ac72df689c75f1aa07bc4e0da784667163
kmyuhkyuk <2451614940@qq.com>
2024-04-04 05:51:11 +0800

    Update
    Former-commit-id: 6f32d2c8bba8a55f03230ab89b8655f835b47eb8

--- GamePanelHUDCompass/Patches/CoopOnBoxLand.cs
+++ GamePanelHUDCompass/Patches/CoopOnBoxLand.cs
@@ -1,7 +1,7 @@
 ﻿#if !UNITY_EDITOR
 
-using EFT.Interactive;
 using UnityEngine;
+using static EFTApi.EFTHelpers;
 
 namespace GamePanelHUDCompass
 {
@@ -9,11 +9,12 @@ namespace GamePanelHUDCompass
     {
         // ReSharper disable once SuggestBaseTypeForParameter
         // ReSharper disable once InconsistentNaming
-        private static void CoopOnBoxLand(MonoBehaviour __instance, object ___boxSync, LootableContainer ___Container)
+        private static void CoopOnBoxLand(MonoBehaviour __instance, object ___boxSync)
         {
             GetNameDescriptionKey(___boxSync, out var nameKey, out var descriptionKey);
 
-            ShowAirdrop(__instance.transform.position, nameKey, descriptionKey, ___Container);
+            ShowAirdrop(__instance.transform.position, nameKey, descriptionKey,
+                _AirdropBoxHelper.RefCoopContainer?.GetValue(__instance));
         }
     }
 }
